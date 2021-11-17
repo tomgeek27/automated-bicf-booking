f5d07781b6e8ec707d401bccd3fb782feef51064
tomgeek27 <tommaso.amadori.dev@gmail.com>
2021-11-17 23:35:22 +0100

    handled errors

--- src/bot.js
+++ src/bot.js
@@ -58,6 +58,22 @@ async function book(page, il_ragazzo, hour) {
   const servizio = await page.$('#servizio')
   await servizio.selectOption("50")
 
+  await page.type("#codice_fiscale", il_ragazzo.codice_fiscale)
+  await page.type("#cognome_nome", il_ragazzo.cognome_nome)
+  await page.type("#email", il_ragazzo.email)
+
+  await page.click("#verify")
+  try {
+    await page.click(`text=${hour}`, {
+      timeout: 1000
+    })
+  } catch (err) {
+    console.error(`${il_ragazzo.cognome_nome} posto gia' prenotato o posti finiti (ore: ${hour})`)
+    return
+  }
+  await page.click(`text=${hour}`).catch(function (r) {console.log});
+  await page.click("#conferma")
+
   console.log(`${il_ragazzo.cognome_nome} prenotato alle ${hour}`)
 }
 
@@ -67,11 +83,11 @@ async function main () {
   const ORE_DIECI = '10:00'
   const ORE_QUINDICI = '15:00'
 
-  const browser = await chromium.launch({headless: false, slowMo: 100});
+  const browser = await chromium.launch({headless: true, slowMo: 0});
   const page = await browser.newPage();
 
   console.log("STARTED\n")
-
+  
   for(const il_ragazzo of i_ragazzi) {
     await book(page, il_ragazzo, ORE_DIECI)
     await book(page, il_ragazzo, ORE_QUINDICI)
