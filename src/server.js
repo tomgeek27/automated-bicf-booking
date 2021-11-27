14fb5c60f6115dc93fbbfc93a375ae076199199b
tomgeek27 <tommaso.amadori.dev@gmail.com>
2021-11-27 12:54:48 +0100

    ready to deploy server

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ const express = require('express')
 const main = require('./bot')
 
 const app = express()
-
-
+app.use(express.static('.'))
 app.get('/normal', (req, res) => {
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.setHeader('Transfer-Encoding', 'chunked');
@@ -18,4 +17,6 @@ app.get('/lm', (req, res) => {
 
 })
 
-app.listen(4000)
+let port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`STARTED AT ${port}`))
